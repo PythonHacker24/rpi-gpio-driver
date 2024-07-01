ccdc2c742c0b02a820748319048817512e78227b
PythonHacker24 <adityapatil24680@gmail.com>
2024-07-01 16:39:34 +0530

    GPIO DIRECT MEMORY ACCESS ADDED

--- .Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/maverick/gpio_driver/Module.symvers :=  scripts/mod/modpost -M -m -a      -o /home/maverick/gpio_driver/Module.symvers -T /home/maverick/gpio_driver/modules.order -i Module.symvers -e 
--- .modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/maverick/gpio_driver/modules.order := {   echo /home/maverick/gpio_driver/gpio_driver.o; :; } > /home/maverick/gpio_driver/modules.order
